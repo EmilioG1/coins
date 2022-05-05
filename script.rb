3465050a89d83e7b4c34b805be26a4111448ec09
Emilio Gonzales <emiliog1184@gmail.com>
2022-05-05 12:01:59 -0700

    add script fix

--- script.rb
+++ script.rb
@@ -3,6 +3,7 @@ require('./lib/coins')
 
 puts 'hello type a number'
 string = gets.chomp
-integer = string.to_int 
+integer = string.to_f
+puts integer
 cash = Coins.new(integer)
 puts cash.money_converter()
